127c7357ab02231a9eaaf0580879613d2a8e3ca4
da0ran9e <98570451+da0ran9e@users.noreply.github.com>
2024-04-02 16:34:11 +0700

    [OBJECT] Define map layer objects

--- SDL2Project/SDL/src/GameObject/Map.cpp
+++ SDL2Project/SDL/src/GameObject/Map.cpp
@@ -1,1 +1,149 @@
+#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+
+#include "GameObject/TextureManager.h"
+#include "GameObject/Sprite2D.h"
+#include "ResourceManagers.h"
 #include "Map.h"
+
+std::string GenerateChunk() {
+    std::string result;
+    const int totalPercentage = 100;
+    const int zeroPercentage = 80;
+    const int onePercentage = 20;
+
+    srand(time(nullptr));
+
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        int randNum = rand() % totalPercentage;
+        if (randNum < zeroPercentage) {
+            result += '0';
+        }
+        else if (randNum < zeroPercentage + onePercentage) {
+            result += '1';
+        }
+    }
+
+    return result;
+}
+
+std::string GenerateObject(std::string chunkData) {
+    std::string result;
+    const int totalPercentage = 100;
+    const int zeroPercentage = 60;
+    const int onePercentage = 9;
+    const int twoPercentage = 8;
+    const int threePercentage = 5;
+    const int fourPercentage = 2;
+    const int fivePercentage = 5;
+    const int sixPercentage = 2;
+    const int sevenPercentage = 8;
+
+    srand(time(nullptr));
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        if (chunkData[i] == 1) {
+            int randNum = rand() % totalPercentage;
+            if (randNum < onePercentage) {
+                result += '1';
+            }
+            else if (randNum < onePercentage + twoPercentage) {
+                result += '2';
+            }
+            else if (randNum < onePercentage + twoPercentage + threePercentage) {
+                result += '3';
+            }
+            else if (randNum < onePercentage + twoPercentage + threePercentage + fourPercentage) {
+                result += '4';
+            }
+            else if (randNum < onePercentage + twoPercentage + threePercentage + fourPercentage + fivePercentage) {
+                result += '5';
+            }
+            else if (randNum < onePercentage + twoPercentage + threePercentage + fourPercentage + fivePercentage + sixPercentage) {
+                result += '6';
+            }
+            else {
+                result += '7';
+            }
+        }
+        else {
+            result += '0';
+        }
+        
+    }
+    return result;
+}
+
+MapChunk::MapChunk()
+{
+    auto texture = ResourceManagers::GetInstance()->GetTexture("Forest_Turf_Texture.png");
+    plainTerrain = std::make_shared<GridPoint>(texture);
+    plainTerrain->terrain = MTerrain::MTERRAIN_PLAIN;
+    texture = ResourceManagers::GetInstance()->GetTexture("Blue_Fungus_Turf_Texture.png");
+    riverTerrain = std::make_shared<GridPoint>(texture);
+    riverTerrain->terrain = MTerrain::MTERRAIN_RIVER;
+    std::string data = GenerateChunk();
+    std::string objectData = GenerateObject(data);
+
+    for (int i = 0; i < CHUNK_SIZE; ++i) {
+        MObject O = MObject::MOBJECT_INVALID;
+        std::shared_ptr<MapObject> newMObject;
+        std::shared_ptr<GridPoint> newGrid;
+        
+        if (data[i] == '0') newGrid = plainTerrain;
+        else if (data[i] == '1') newGrid = riverTerrain;
+      
+        if (objectData[i] == '0') O = MObject::MOBJECT_INVALID;
+        else if (objectData[i] == '1') O = MObject::MOBJECT_TREE;
+        else if (objectData[i] == '2') O = MObject::MOBJECT_BUSH;
+        else if (objectData[i] == '3') O = MObject::MOBJECT_GRASS;
+        else if (objectData[i] == '4') O = MObject::MOBJECT_CROP;
+        else if (objectData[i] == '5') O = MObject::MOBJECT_CHESS;
+        else if (objectData[i] == '6') O = MObject::MOBJECT_ROCK;
+       
+        newGrid->gridNumber = i;
+        grids.push_back(newGrid);
+    }
+}
+
+void MapChunk::Draw(SDL_Renderer* renderer)
+{
+    for (auto it : grids)
+    {
+            it->Draw(renderer);
+    }
+}
+
+Map::Map() {
+    h = 3;
+    v = 3;
+    std::shared_ptr<MapChunk> newMapChunk = std::make_shared<MapChunk>();
+    chunks.push_back(newMapChunk);
+}
+
+void Map::Draw(SDL_Renderer* renderer)
+{
+    for (auto it : chunks)
+    {
+        it->Draw(renderer);
+    }
+}
+
+GridPoint::GridPoint()
+{
+ }
+
+GridPoint::GridPoint(std::shared_ptr<TextureManager> texture)
+{
+
+}
+
+void GridPoint::Draw(SDL_Renderer* renderer)
+{
+    //Get2DPosition();
+    if (texture != nullptr)
+    {
+        texture->Render(50 * (gridNumber % 1024), 50 * (gridNumber / 1024), 50, 50, 0, m_flip);
+    }
+}
--- SDL2Project/SDL/src/GameObject/Map.h
+++ SDL2Project/SDL/src/GameObject/Map.h
@@ -3,20 +3,52 @@
 #include<vector>
 
 #include"BaseObject.h"
+#include"Sprite2D.h"
+#include"MapObject.h"
+#include"Mob.h"
 #include"CMath.h"
 #include"Define.h"
 
-struct GridPoint{
-	int terrain; //terrain type
-	//std::vector <int> objects; //list the objects that are on this map
-	//std::vector <int> entities;
-	float light[3]; //R,G,B
-	int creature[MAX_CREATURES];
+enum class MTerrain
+{
+	MTERRAIN_INVALID = 0,
+	MTERRAIN_PLAIN,
+	MTERRAIN_RIVER
+};
+
+class GridPoint : Sprite2D{
+public:
+	int gridNumber;
+	MTerrain terrain; //terrain type
+	std::shared_ptr<TextureManager> texture = ResourceManagers::GetInstance()->GetTexture("Forest_Turf_Texture.png");
+
+	GridPoint();
+	GridPoint(std::shared_ptr<TextureManager> texture);
+
+	void Draw(SDL_Renderer* renderer) override;
+};
+
+class MapChunk : Sprite2D {
+public:
+	std::shared_ptr<GridPoint> plainTerrain;
+	std::shared_ptr<GridPoint> riverTerrain;
+	std::vector<std::shared_ptr<GridPoint>> grids; //list the map grid
+	std::vector< std::shared_ptr<MapObject>> objects; //list the objects that are here
+	//std::vector<Mob> Mobs; // list the mobs
+
+	MapChunk();
+	//MapChunk(char* data);
+	void Draw(SDL_Renderer* renderer) override;
 };
 
 class Map
 {
 public:
+	int h, v; //size of the map in chunk unit
+	std::vector<std::shared_ptr<MapChunk>> chunks;
 
+	Map();
+	//Map(char* data);
+	void Draw(SDL_Renderer* renderer);
 };
 
