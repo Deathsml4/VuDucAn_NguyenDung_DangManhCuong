76b6e58ca34683e3a0f92538f025fb9f409306c8
Vũ Đức An <98570451+da0ran9e@users.noreply.github.com>
2024-04-25 18:54:51 +0700

    [GAMEOVER] done with message

--- SDL2Project/SDL/src/GameObject/GameOver.cpp
+++ SDL2Project/SDL/src/GameObject/GameOver.cpp
@@ -1,12 +1,37 @@
 #include "GameOver.h"
 
-GameOver::GameOver(std::string time, std::string layout)
+GameOver::GameOver(std::string time, std::string layer)
 {
 	auto texture = ResourceManagers::GetInstance()->GetTexture("imgpsh_fullsize_anim.png");
 	textBox = std::make_shared<Sprite2D>(texture, SDL_FLIP_NONE);
 	textBox->SetSize(SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
 	textBox->Set2DPosition(SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4);
 	drawables.push_back(textBox);
+
+	///Set Font
+	title = std::make_shared<Text>("Data/Text/Consolas.ttf", textColorBlack);
+	title->SetSize(120, 40);
+	title->Set2DPosition(SCREEN_WIDTH / 4 + (SCREEN_WIDTH / 2 - title->GetWidth())/2, SCREEN_HEIGHT / 4 + 60);
+	title->LoadFromRenderText("Gameover!");
+	drawables.push_back(title);
+
+	message = std::make_shared<Text>("Data/Text/Consolas.ttf", textColorBlack);
+	message->SetSize(300, 30);
+	message->Set2DPosition(SCREEN_WIDTH / 4 + (SCREEN_WIDTH / 2 - message->GetWidth()) / 2, SCREEN_HEIGHT / 4 + 90);
+	message->LoadFromRenderText("Your adventure ended here!");
+	drawables.push_back(message);
+
+	finishTime = std::make_shared<Text>("Data/Text/Consolas.ttf", textColorRed);
+	finishTime->SetSize(90, 20);
+	finishTime->Set2DPosition(SCREEN_WIDTH / 4 + (SCREEN_WIDTH / 2 - finishTime->GetWidth()) / 2, SCREEN_HEIGHT / 4 + 130);
+	finishTime->LoadFromRenderText("Survived " + time);
+	drawables.push_back(finishTime);
+
+	finishLayer = std::make_shared<Text>("Data/Text/Consolas.ttf", textColorRed);
+	finishLayer->SetSize(90, 20);
+	finishLayer->Set2DPosition(SCREEN_WIDTH / 4 + (SCREEN_WIDTH / 2 - finishLayer->GetWidth()) / 2, SCREEN_HEIGHT / 4 + 210);
+	finishLayer->LoadFromRenderText("Passed " + layer + " pages");
+	drawables.push_back(finishLayer);
 }
 
 GameOver::~GameOver()
--- SDL2Project/SDL/src/GameObject/GameOver.h
+++ SDL2Project/SDL/src/GameObject/GameOver.h
@@ -21,14 +21,15 @@ public:
 	SDL_Color textColorBlack = { 0, 0, 0 };
 	std::shared_ptr<Sprite2D> textBox;
 	std::shared_ptr<Sprite2D> imageA;
-	std::shared_ptr<Text> Title;
+	std::shared_ptr<Text> title;
+	std::shared_ptr<Text> message;
 	std::shared_ptr<Text> finishTime;
 	std::shared_ptr<Text> finishLayer;
 	std::shared_ptr<MouseButton> RestartBtm;
 	std::shared_ptr<MouseButton> ExitBtn;
 	std::list< std::shared_ptr<BaseObject>> drawables;
 
-	GameOver(std::string time, std::string layout);
+	GameOver(std::string time, std::string layer);
 	~GameOver();
 
 	void Init();
